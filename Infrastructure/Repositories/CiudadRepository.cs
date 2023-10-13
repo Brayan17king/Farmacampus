88f025915a78c20c0d2c3fc55b1de3f2c0a6aa40
Brayan17king <xbrayansneyder@gmail.com>
2023-10-13 12:18:24 -0500

    feat: :sparkles: Creation of repositories with their methods and properties

---
+++ Infrastructure/Repositories/CiudadRepository.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class CiudadRepository : GenericRepository<Ciudad>,ICiudadRepository
+{
+    private readonly FarmaContext _context;
+
+    public CiudadRepository(FarmaContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public override async Task<IEnumerable<Ciudad>> GetAllAsync()
+    {
+        return await _context.Ciudades
+                    .Include(c => c.UbicacionPersonas)
+                    .ToListAsync();
+    }
+
+    public override async Task<(int totalRegistros, IEnumerable<Ciudad> registros)> GetAllAsync(
+        int pageIndex,
+        int pageSize,
+        string search
+    )
+    {
+        var query = _context.Ciudades as IQueryable<Ciudad>;
+    
+        if (!string.IsNullOrEmpty(search))
+        {
+            query = query.Where(p => p.NombreCiudad.ToLower().Contains(search)); // If necesary add .ToString() after varQuery
+        }
+        query = query.OrderBy(p => p.Id);
+    
+        var totalRegistros = await query.CountAsync();
+        var registros = await query
+                        .Include(p => p.UbicacionPersonas)
+                        .Skip((pageIndex - 1) * pageSize)
+                        .Take(pageSize)
+                        .ToListAsync();
+        return (totalRegistros, registros);
+    }
+}
---
+++ Infrastructure/Repositories/DepartamentoRepository.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+public class DepartamentoRepository : GenericRepository<Departamento>, IDepartamentoRepository
+{
+    private readonly FarmaContext _context;
+
+    public DepartamentoRepository(FarmaContext context) : base(context)
+    {
+        _context = context;
+    }
+     public override async Task<IEnumerable<Departamento>> GetAllAsync()
+    {
+        return await _context.Departamentos
+                    .Include(c => c.Ciudades)
+                    .ToListAsync();
+    }
+
+    public override async Task<(int totalRegistros, IEnumerable<Departamento> registros)> GetAllAsync(
+        int pageIndex,
+        int pageSize,
+        string search
+    )
+    {
+        var query = _context.Departamentos as IQueryable<Departamento>;
+    
+        if (!string.IsNullOrEmpty(search))
+        {
+            query = query.Where(p => p.NombreDepartamento.ToLower().Contains(search)); // If necesary add .ToString() after varQuery
+        }
+        query = query.OrderBy(p => p.Id);
+    
+        var totalRegistros = await query.CountAsync();
+        var registros = await query
+                        .Include(p => p.Ciudades)
+                        .Skip((pageIndex - 1) * pageSize)
+                        .Take(pageSize)
+                        .ToListAsync();
+        return (totalRegistros, registros);
+    }
+}
