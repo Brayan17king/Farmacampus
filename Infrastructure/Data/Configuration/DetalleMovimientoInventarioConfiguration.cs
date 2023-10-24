6e5f6ba24981f66708b2fc9f69a0f9ce61ce6daa
Brayan17king <104658332+Brayan17king@users.noreply.github.com>
2023-10-24 16:06:33 -0500

    feat: :memo: Configuration Entities Correccion data

--- Infrastructure/Data/Configuration/DetalleMovimientoInventarioConfiguration.cs
+++ Infrastructure/Data/Configuration/DetalleMovimientoInventarioConfiguration.cs
@@ -14,14 +14,17 @@ public class DetalleMovimientoInventarioConfiguration : IEntityTypeConfiguration
     {
         builder.ToTable("detallemovimiento");
 
-        builder.HasKey(c => new {c.IdInventarioFk, c.IdMovimientoInventarioFk});
-        builder.Property(c => new {c.IdInventarioFk, c.IdMovimientoInventarioFk});
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id);
 
         builder.Property(x => x.Cantidad).HasColumnType("int");
 
         builder.Property(x => x.Precio).HasColumnType("double");
 
+        builder.Property(x => x.IdInventarioFk).HasMaxLength(50);
         builder.HasOne(x => x.Inventarios).WithMany(x => x.DetalleMovimientoInventarios).HasForeignKey(x => x.IdInventarioFk);
+        
+        builder.Property(x => x.IdMovimientoInventarioFk).HasMaxLength(50);
         builder.HasOne(x => x.MovimientoInventarios).WithMany(x => x.DetalleMovimientoInventarios).HasForeignKey(x => x.IdMovimientoInventarioFk);
     }
 }
